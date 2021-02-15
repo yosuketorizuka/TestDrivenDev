849b0dc5b2e6e71e6ede30f448e2f696687c85fe
yosuketorizuka <66625403+yosuketorizuka@users.noreply.github.com>
2021-02-15 21:50:21 +0900

    update_20210215001

--- tddPractice/money/MoneyTest.py
+++ tddPractice/money/MoneyTest.py
@@ -4,14 +4,17 @@ import unittest
 import dollar
 
 # クラス名はなんでもよいが、unittest.TestCaseの継承は必要
-class DollarTestCase(unittest.TestCase):
+class MoneyTest(unittest.TestCase):
 
-    def test_dollar(self):
+    def testMutiplication(self):
            
         five = dollar.Dollar(5)
+        
         product = five.times(2)
-
         self.assertEqual(10, product.amount)
+
+        product = five.times(3)
+        self.assertEqual(15, product.amount)
 
 if __name__ == '__main__':
     unittest.main()
